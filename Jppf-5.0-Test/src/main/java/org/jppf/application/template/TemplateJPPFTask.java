40bba756b227eef6f29279f3adafe17865215825
miguelveces <mveces8@pancanal.com>
2015-10-05 17:57:15 -0500

    se genero una segunda tarea para probar el admin del JPPF

--- Jppf-5.0-Test/src/main/java/org/jppf/application/template/TemplateJPPFTask.java
+++ Jppf-5.0-Test/src/main/java/org/jppf/application/template/TemplateJPPFTask.java
@@ -20,50 +20,66 @@ package org.jppf.application.template;
 import org.jppf.node.protocol.AbstractTask;
 
 /**
- * This class is a template for a standard JPPF task.
- * There are 3 parts to a task that is to be executed on a JPPF grid:
+ * This class is a template for a standard JPPF task. There are 3 parts to a
+ * task that is to be executed on a JPPF grid:
  * <ol>
- * <li>the task initialization: this is done on the client side, generally from the task constructor,
- * or via explicit method calls on the task from the application runner.</li>
- * <li>the task execution: this part is performed by the node. It consists in invoking the {@link #run() run()} method,
- * and handling an eventual uncaught {@link java.lang.Throwable Throwable} that would result from this invocation.</li>
- * <li>getting the execution results: the task itself, after its execution, is considered as the result.
- * JPPF provides the convenience methods {@link org.jppf.node.protocol.Task#setResult(java.lang.Object) setResult(Object)} and
- * {@link org.jppf.server.node.Task#getResult() getResult()}
- * to this effect, however any accessible attribute of the task will be available when the task is returned to the client.</li>
+ * <li>the task initialization: this is done on the client side, generally from
+ * the task constructor, or via explicit method calls on the task from the
+ * application runner.</li>
+ * <li>the task execution: this part is performed by the node. It consists in
+ * invoking the {@link #run() run()} method, and handling an eventual uncaught
+ * {@link java.lang.Throwable Throwable} that would result from this
+ * invocation.</li>
+ * <li>getting the execution results: the task itself, after its execution, is
+ * considered as the result. JPPF provides the convenience methods
+ * {@link org.jppf.node.protocol.Task#setResult(java.lang.Object) setResult(Object)}
+ * and {@link org.jppf.server.node.Task#getResult() getResult()} to this effect,
+ * however any accessible attribute of the task will be available when the task
+ * is returned to the client.</li>
  * </ol>
+ *
  * @author Laurent Cohen
  */
 public class TemplateJPPFTask extends AbstractTask<String> {
-  /**
-   * Perform initializations on the client side,
-   * before the task is executed by the node.
-   */
-  public TemplateJPPFTask() {
-    // perform initializations here ...
-  }
 
-  /**
-   * This method contains the code that will be executed by a node.
-   * Any uncaught {@link java.lang.Throwable Throwable} will be stored in the task via a call to {@link org.jppf.node.protocol.Task#setThrowable(java.lang.Throwable) Task.setThrowable(Throwable)}.
-   */
-  @Override
-  public void run() {
-      // write your task code here.
-    System.out.println("Hola esta es la imprecion de la tarea 1");
-   
+    /**
+     * Perform initializations on the client side, before the task is executed
+     * by the node.
+     */
+    public TemplateJPPFTask() {
+        // perform initializations here ...
+    }
+
+    /**
+     * This method contains the code that will be executed by a node. Any
+     * uncaught {@link java.lang.Throwable Throwable} will be stored in the task
+     * via a call to
+     * {@link org.jppf.node.protocol.Task#setThrowable(java.lang.Throwable) Task.setThrowable(Throwable)}.
+     */
+    @Override
+    public void run() {
+        // write your task code here.
+        System.out.println("Hola esta es la imprecion de la tarea 1");
+
     // ...
+        // eventually set the execution results
+        int s = 100;
 
-    // eventually set the execution results
-    
-    int s = 100;
-    
-   int ss =1933;
-   
-   double sd = s*ss;
-   
-   String mensaje = sd>100?"hola esta es una tarea cierta":"esta es una tarea falsa";
-      System.out.println(mensaje);
-    setResult(mensaje);
-  }
+        int ss = 1933;
+
+        double sd = s * ss;
+        // simply wait for 3 seconds
+        String mensaje = "";
+        try {
+            Thread.sleep(3000L);
+            mensaje = sd > 100 ? "hola esta es una tarea cierta" : "esta es una tarea falsa";
+            System.out.println(mensaje);
+        } catch (InterruptedException e) {
+            setThrowable(e);
+            return;
+        }
+// eventually set the execution results
+
+        setResult(mensaje);
+    }
 }
